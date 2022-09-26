696f4e9ec150365308d59f4898bcd3b6a1fb5079
olaide-hok <olaidekareem43@yahoo.com>
2022-09-26 11:38:09 +0100

    Create object type example in object-type.ts

---
+++ Typescript Types/object-type.ts
@@ -0,0 +1,10 @@
+// Specifying object (key-value pairs) type as key-type pairs.
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Maximilian",
+  age: 30,
+};
+
+console.log(person.name);
