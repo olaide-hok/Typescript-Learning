9cc0730df9b27dd5b49bc07fa0584ca44ec06b44
olaide-hok <olaidekareem43@yahoo.com>
2022-09-27 08:31:30 +0100

    Create type aliase example

---
+++ Typescript Types/type-aliasing.ts
@@ -0,0 +1,33 @@
+/**
+ * Example of type aliasing
+ * Here, the type to be used was decalared in a variable named "Combinable".
+ * Which is then referenced in line 11 & 12 as types for input1 and input2
+ * 
+ * Similarly, "ConversionDescriptor" stands as a literal type aliase for "resultConversion" 
+ */
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
