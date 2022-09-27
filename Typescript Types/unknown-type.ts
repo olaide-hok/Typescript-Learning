4f2c978511ec7bf63234643b6482d69768427d3a
olaide-hok <olaidekareem43@yahoo.com>
2022-09-27 08:52:42 +0100

    Create unknown type example.
    Delete unused file

---
+++ Typescript Types/unknown-type.ts
@@ -0,0 +1,20 @@
+// Unknown type decalaration
+
+/**
+ * The type unknown was declared for the userInput variable
+ */
+
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
