13d7da7f962be486dc569aec5915d4d4b24c3397
olaide-hok <olaidekareem43@yahoo.com>
2022-09-27 08:20:46 +0100

    Create literals type example in literals-type.ts

---
+++ Typescript Types/literals-type.ts
@@ -0,0 +1,33 @@
+/**
+ * Literals type decalaration example
+ * @param input1 with union type decalaration
+ * @param input2 with union type decalaration
+ * @param resultConversion with literal type decalration
+ * @returns 
+ */
+
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
