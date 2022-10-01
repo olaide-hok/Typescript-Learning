028424d059095c8a59357e9601ab7fbc40b3083e
olaide-hok <olaidekareem43@yahoo.com>
2022-10-01 20:52:47 +0100

    Create destructuring.ts with example

---
+++ Typescript Next Gen/destructuring.ts
@@ -0,0 +1,28 @@
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: 'Max',
+  age: 30
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
