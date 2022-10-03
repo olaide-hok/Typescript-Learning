780f379bffe1cbcac18534bad8b447a46cc5b8bc
olaide-hok <olaidekareem43@yahoo.com>
2022-10-03 16:18:22 +0100

    Created rest-parameter.ts with example

---
+++ Typescript Next Gen/rest-parameter.ts
@@ -0,0 +1,9 @@
+// Using rest parameter to pass in multiple arguments into the add function
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+      return curResult + curValue;
+    }, 0);
+  };
+  
+  const addedNumbers = add(5, 10, 2, 3.7);
+  console.log(addedNumbers);
