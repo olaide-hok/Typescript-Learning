60632b35d82604eb009068ae50d31d8b663805d8
olaide-hok <olaidekareem43@yahoo.com>
2022-10-03 16:13:51 +0100

    Cleaned out non-spread operator example

--- Typescript Next Gen/spread-operator.ts
+++ Typescript Next Gen/spread-operator.ts
@@ -1,20 +1,4 @@
-const userName = 'Max';
-// userName = 'Maximilian';
-let age = 30;
-
-age = 29;
-
-const add = (a: number, b: number = 1) => a + b;
-
-const printOutput: (a: number | string) => void = output => console.log(output);
-
-const button = document.querySelector('button');
-
-if (button) {
-  button.addEventListener('click', event => console.log(event));
-}
-
-printOutput(add(5));
+// Spread operator example
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
