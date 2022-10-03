36fbb3787aae43151201c7f7d8704f660c46d4af
olaide-hok <olaidekareem43@yahoo.com>
2022-10-03 16:13:04 +0100

    Created arrow-fnc.ts with example

---
+++ Typescript Next Gen/arrow-fnc.ts
@@ -0,0 +1,13 @@
+// Arrow function example
+
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5));
