b597ab1603decf436c46409c64124d2dee9c4285
olaide-hok <olaidekareem43@yahoo.com>
2022-10-03 16:36:24 +0100

    Update rest-parameter with tuple type example

--- Typescript Next Gen/rest-parameter.ts
+++ Typescript Next Gen/rest-parameter.ts
@@ -7,3 +7,13 @@ const add = (...numbers: number[]) => {
   
   const addedNumbers = add(5, 10, 2, 3.7);
   console.log(addedNumbers);
+
+  // Using rest parameter in conjunction with a tuple ie. an array with already know
+  // number of inputs and their respective types
+  const add1 = (...numbers: [number, number, number]) => {
+    return numbers.reduce((curResult, curValue) => {
+      return curResult + curValue;
+    }, 0);
+  };
+
+  const addedNumbers1 = add1(5, 10, 2);
