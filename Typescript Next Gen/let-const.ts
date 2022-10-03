82ac209f070f7aa168effd9964e6594c4f97b75b
olaide-hok <olaidekareem43@yahoo.com>
2022-10-03 16:45:29 +0100

    Cleaned out unsed lines of code

--- Typescript Next Gen/let-const.ts
+++ Typescript Next Gen/let-const.ts
@@ -1,17 +1,8 @@
+// let-const usage example
+
 const userName = 'Max';
 
 let age = 30;
 
 age = 29;
 
-const add = (a: number, b: number) => a + b;
-
-const printOutput: (a: number | string) => void = output => console.log(output);
-
-const button = document.querySelector('button');
-
-if (button) {
-  button.addEventListener('click', event => console.log(event));
-}
-
-printOutput(add(5, 2));
