ece54162475ee74f118a11a387c2f567d4fa4a66
olaide-hok <olaidekareem43@yahoo.com>
2022-10-14 07:25:33 +0100

    Modified readonly example

--- Interfaces/interface-readonly.ts
+++ Interfaces/interface-readonly.ts
@@ -1,29 +1,28 @@
 // A 'readonly' modifier can be added which means it is set once and can't be change once the object has been initialized.
-interface Named {
-  readonly name: string;
-}
 
-interface Greetable extends Named {
+interface Greetable1 {
+  readonly name1: string;
+
   greet(phrase: string): void;
 }
 
-class Person1 implements Greetable {
-  name: string;
+class Person1 implements Greetable1 {
+  name1: string;
   age = 30;
 
   constructor(n: string) {
-    this.name = n;
+    this.name1 = n;
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    console.log(phrase + " " + this.name1);
   }
 }
 
-let user3: Greetable;
+let user3: Greetable1;
 
 user3 = new Person1("Max");
-// user1.name = 'Manu'; this shows that the initial value set cannot be changed due to the readonly modifier.
+// user3.name = 'Manu'; this shows that the initial value set cannot be changed due to the readonly modifier.
 
 user3.greet("Hi there - I am");
-console.log(user1);
+console.log(user3);
