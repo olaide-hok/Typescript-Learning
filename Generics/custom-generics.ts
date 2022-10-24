9f0856e951c0d06143696882211d6e73baf4f883
olaide-hok <olaidekareem43@yahoo.com>
2022-10-24 11:42:42 +0100

    Completed custom generic type creation.

--- Generics/custom-generics.ts
+++ Generics/custom-generics.ts
@@ -1,12 +1,23 @@
 //
 
-function merge(objA: object, objB: object) {
-    return Object.assign(objA, objB)
+// function merge(objA: object, objB: object) {
+//     return Object.assign(objA, objB)
+// }
+
+// console.log(merge({name: 'Max'}, {age: 30}));
+
+// const mergedObj = merge({name: 'Max'}, {age: 30})
+
+// mergedObj.name; // this doesn't work because the mergedObj object returned doesn't contain all the necessary information. 
+// mergedObj.age; //same explanation as above.
+
+function merge<T, U>(objA: T, objB: U) { // the types set here are dynamic
+    return Object.assign(objA, objB); // returns the intersection of T & U
 }
 
-console.log(merge({name: 'Max'}, {age: 30}));
-
 const mergedObj = merge({name: 'Max'}, {age: 30})
+const mergedObj1 = merge({name: 'Max', hobbies: ["Sports"]}, {age: 30})
 
-mergedObj.name; // this doesn't work because the mergedObj object returned doesn't contain all the necessary information. 
-mergedObj.age; //same explanation as above.
+console.log(mergedObj.age);
+
+const mergedObj2 = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Max', hobbies: ["Sports"]}, {age: 30}) // This is a redundant way generics helps prevent i.e. the parameter type specification in the angle brackets after the function name.
