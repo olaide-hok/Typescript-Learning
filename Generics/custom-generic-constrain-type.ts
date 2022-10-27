eaeee19e92410dfbf293fc3a1c8463016a586499
olaide-hok <olaidekareem43@yahoo.com>
2022-10-27 12:55:02 +0100

    Created custom generic using typeof constraint.

---
+++ Generics/custom-generic-constrain-type.ts
@@ -0,0 +1,7 @@
+// Using the 'typeof' constraint
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value:' + obj[key];
+}
+
+extractAndConvert({ name: 'Max'}, 'name');
