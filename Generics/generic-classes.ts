7379467dda988aadcc3a85282c1258f89ca33e99
olaide-hok <olaidekareem43@yahoo.com>
2022-10-28 13:34:21 +0100

    Extends DataStorage to string, number & bool only

--- Generics/generic-classes.ts
+++ Generics/generic-classes.ts
@@ -1,27 +1,29 @@
+class DataStorage<T extends string | number | boolean > {
+  private data: T[] = [];
 
-class DataStorage<T> {
-    private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
 
-    addItem(item: T) {
-        this.data.push(item);
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      // indexof returns -1 if it doesn't find anything in the array.
+      return;
     }
+    this.data.splice(this.data.indexOf(item), 1); // -1
+  }
 
-    removeItem(item: T) {
-        this.data.splice(this.data.indexOf(item), 1)
-    }
-
-    getItems() {
-        return [...this.data];
-    }
+  getItems() {
+    return [...this.data];
+  }
 }
 
-const textStorage = new DataStorage<string>()
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.addItem('Banana')
-textStorage.removeItem('Max');
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.addItem("Banana");
+textStorage.removeItem("Max");
 console.log(textStorage.getItems());
-
 
 const numberStorage = new DataStorage<number>();
 numberStorage.addItem(10);
@@ -29,3 +31,7 @@ numberStorage.addItem(23);
 numberStorage.addItem(56);
 numberStorage.removeItem(100);
 console.log(numberStorage.getItems());
+
+//Object in Javascript are reference types as against primitive types
+
+// Methods in classes can as well use generic types and constrains specific to them.
