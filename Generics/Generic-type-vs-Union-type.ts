46bd960d0833a4c059c07613bc469f5210586821
olaide-hok <olaidekareem43@yahoo.com>
2022-10-31 09:00:49 +0100

    Doc'd difference between Union type & Generic type

---
+++ Generics/Generic-type-vs-Union-type.ts
@@ -0,0 +1,8 @@
+/**
+ * Generic Type vs Union Type
+ * 
+ * Union type allows for different values with different types.
+ * Union type brings flexibility when in a class instance or in function call
+ * 
+ * Generic types allows for type specificity i.e. lock in a specific type when either in a class instance or a function call.
+ */
