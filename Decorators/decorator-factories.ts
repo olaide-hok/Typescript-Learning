236f522f401b7bc200126d8b08d01cdd01234256
olaide-hok <olaidekareem43@yahoo.com>
2022-11-06 08:15:56 +0100

    Added explanantion of decorator factories.

--- Decorators/decorator-factories.ts
+++ Decorators/decorator-factories.ts
@@ -1,3 +1,9 @@
+/**
+ * A decorator factory returns a decorator function which allows for configuration when assigned as a decorator to something. Allows passing of values to be passed to the decorator function.
+ * @param logString a string parameter
+ * @returns a decorator function
+ */
+
 function LoggerA(logString: string) {
   return function (constructor: Function) {
     console.log(logString);
