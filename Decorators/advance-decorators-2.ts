7e29e74ab8a7d82c4a4161d1b26f38c85e6c0e59
olaide-hok <olaidekareem43@yahoo.com>
2022-11-08 16:05:20 +0100

    Added explanation to adv-decorators-2.ts

--- Decorators/advance-decorators-2.ts
+++ Decorators/advance-decorators-2.ts
@@ -1,4 +1,4 @@
-function Logger(logString: string) {
+function Logger1(logString: string) {
   console.log("LOGGER FACTORY");
   return function (constructor: Function) {
     console.log(logString);
@@ -6,7 +6,7 @@ function Logger(logString: string) {
   };
 }
 
-function WithTemplate(template: string, hookId: string) {
+function WithTemplate1(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
   return function (constructor: any) {
     console.log("Rendering template");
@@ -19,9 +19,13 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-// @Logger('LOGGING - PERSON')
-@Logger("LOGGING")
-@WithTemplate("<h1>My Person Object</h1>", "app")
+
+/**
+ * The order in which decorators functions executes is bottom-up. The creation of the decorator factories executes as specified (i.e. line by line.)
+ */
+
+@Logger1("LOGGING")
+@WithTemplate1("<h1>My Person Object</h1>", "app")
 class Person4 {
   name = "Max";
 
