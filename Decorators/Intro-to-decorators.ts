6216dc152218139a25d58111e5a98b20536a86c6
olaide-hok <olaidekareem43@yahoo.com>
2022-11-13 08:18:00 +0100

    Added how decorator work to intro-to-decorators.ts

--- Decorators/Intro-to-decorators.ts
+++ Decorators/Intro-to-decorators.ts
@@ -5,3 +5,10 @@
  * 
  * Helps guarantee that classes, implementations etc are correctly used.
  */
+
+/**
+ * How Decorators works
+ * It is not the instantiation of the class that matters, they don't run at runtime. They allow for behind the scenes setup work when a class is defined.
+ * 
+ * They are not event listeners. A decorator is a function that executes when a class is defined, when a method is registered etc. to store extra metadata about the class/method/accessor(getter or setter)/parameter.
+ */
