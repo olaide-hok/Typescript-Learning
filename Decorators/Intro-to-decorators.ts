36d02cb1592380a4d5ce57fd9f6331b9caeec7a2
olaide-hok <olaidekareem43@yahoo.com>
2022-11-14 09:42:24 +0100

    Added more explantion on decorators

--- Decorators/Intro-to-decorators.ts
+++ Decorators/Intro-to-decorators.ts
@@ -7,8 +7,28 @@
  */
 
 /**
- * How Decorators works
+ * How Decorators executes
  * It is not the instantiation of the class that matters, they don't run at runtime. They allow for behind the scenes setup work when a class is defined.
  * 
  * They are not event listeners. A decorator is a function that executes when a class is defined, when a method is registered etc. to store extra metadata about the class/method/accessor(getter or setter)/parameter.
+ */
+
+/**
+ * Other Decorator Return Types
+ * 
+ * Decorators added to Methods and Accessor can return something that can be used by Typescript e.g.Log2 and Log3
+ * 
+ * Log2 and Log3 can return a brand new Property Descriptor. The Property Descriptor is a JavaScript thing.
+ * 
+ * For Accessor Decorators, it as the configurable(bool), enumerable(bool), get(fn), and set(fn).
+ * 
+ * For Method Decorators, it as the configurable(bool), enumerable(bool), value (fn return value), writable(bool).
+ * 
+ * Configurable - if the configurration can change after creation or can be deleted.
+ * Enumerable - if an operation can be performed on it such as looping.
+ * Writable - if it can be changed after the object has been created.
+ * 
+ * Decorators added to Properties and Parameters can also return something but will be ignored by Typescript i.e. they will not be used.
+ * 
+ * 
  */
