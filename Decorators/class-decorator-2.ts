30afe9a2546a8cc5886658daa7edbc5ab7e90d69
olaide-hok <olaidekareem43@yahoo.com>
2022-11-14 08:53:00 +0100

    Created a class in class decorator example

---
+++ Decorators/class-decorator-2.ts
@@ -0,0 +1,31 @@
+/**
+ * Adding a return value inside the decorator function
+ */
+
+function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function <T extends { new (...args: any[]): {name: string} }> (
+        originalConstructor: T
+    ) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('Rendering template');
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name
+                }                
+            }
+        }
+    }    
+}
+
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Personn {
+    name = 'Max';
+
+    constructor() {
+        console.log('Creating person object...');
+    }
+}
