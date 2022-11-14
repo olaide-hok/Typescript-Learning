fa1e7d42e158fff517b5a896faff38b532d18ed5
olaide-hok <olaidekareem43@yahoo.com>
2022-11-14 14:22:18 +0100

    Created a custom autobind decorator.
    The Autobind Decorator returns a custom descriptor.

--- Decorators/custom-autobind-decorator.ts
+++ Decorators/custom-autobind-decorator.ts
@@ -1,8 +1,22 @@
+// Autobind decorator
 
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        },
+    }
+    return adjDescriptor
+}
 
 class Printer {
     message = 'This works!';
 
+    @Autobind
     showMessage() {
         console.log(this.message);        
     }
@@ -10,4 +24,16 @@ class Printer {
 
 const p = new Printer();
 const buttonn = document.querySelector('button')!;
-buttonn.addEventListener('click', p.showMessage.bind(p))
+buttonn.addEventListener('click', p.showMessage)
+
+// class Printer {
+//     message = 'This works!';
+
+//     showMessage() {
+//         console.log(this.message);        
+//     }
+// }
+
+// const p = new Printer();
+// const buttonn = document.querySelector('button')!;
+// buttonn.addEventListener('click', p.showMessage.bind(p))
