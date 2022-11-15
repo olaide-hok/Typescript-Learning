c56b1ab75b7560e3435e86edd4fa781117292b4e
olaide-hok <olaidekareem43@yahoo.com>
2022-11-15 15:26:43 +0100

    Complete Validation Decorator

--- Decorators/validation-decorator.ts
+++ Decorators/validation-decorator.ts
@@ -1,7 +1,54 @@
 // Decorators for Validation
 
+interface ValidatorConfig {
+    [property: string]: {
+        [validatableProp: string]: string[] // ['required', 'positive']
+    }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['required']
+    }
+}
+
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['positive']
+    }
+}
+
+function validate(obj: any) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name]
+    if (!objValidatorConfig) {
+        return true
+    }
+    let isValid = true
+    for (const prop in objValidatorConfig) {
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop]
+                    break                 
+                case 'positve':
+                    isValid = isValid && obj[prop] > 0
+                    break
+                default:
+                    break;
+            }
+        }
+    }
+    return isValid
+}
+
 class Course {
+    @Required
     title: string
+    @PositiveNumber
     price: number
 
     constructor(t: string, p: number) {
