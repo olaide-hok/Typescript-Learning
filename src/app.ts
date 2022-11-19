4bc6b23b0595003b7ba699d78146f6e1d0e6688d
olaide-hok <olaidekareem43@yahoo.com>
2022-11-19 13:33:58 +0100

    Created DOM element variable in class constructor

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
@@ -9,6 +12,15 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+
+        // Adds the 'user-input' id to the form div
+        this.element.id = 'user-input'
+
+        // Page Input Elements
+        this.titleInputElement = document.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = document.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = document.querySelector('#people') as HTMLInputElement
+
         this.attach()
     }
     
