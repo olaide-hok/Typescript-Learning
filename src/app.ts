5a9c441dd897feec05cd6a4ebff757effd4fe7ef
olaide-hok <olaidekareem43@yahoo.com>
2022-11-24 16:32:17 +0100

    Complete drag & drop feature.
    Create updateListeners as a private mtd for reuse in ProjectState class.
    
    Create moveProject method in ProjectState class to manage project status
    
    Complete dropHandler mtd in ProjectList class that handles project stat

--- src/app.ts
+++ src/app.ts
@@ -67,6 +67,18 @@ class ProjectState extends State<Project> {
         )
         
         this.projects.push(newProject)
+        this.updateListeners()
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId)
+        if (project && project.status !== newStatus) {
+            project.status = newStatus
+            this.updateListeners()
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice())
         }
@@ -211,8 +223,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @Autobind
     dropHandler(event: DragEvent): void {
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
     }
 
     @Autobind
