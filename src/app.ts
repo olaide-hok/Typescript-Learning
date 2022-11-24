eb10a495643527338646646a83f7c2cd495fb0bb
olaide-hok <olaidekareem43@yahoo.com>
2022-11-24 15:24:40 +0100

    Add Drag Events and reflect UI State changes.
    ProjectList class implements DragTarget interface.
    
    dragOverHandler and dragLeaveHandler is implemented.
    
    dragOverHandler and dragLeaveHandler uses the Autobind decorator.
    
    ProjectList configure mtd adds dragover, dragleave & drop evnt listeners

--- src/app.ts
+++ src/app.ts
@@ -191,7 +191,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 }
 
 // ProjectList Class
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[]
 
     constructor(private type: 'active' | 'finished') {
@@ -202,7 +202,27 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         this.renderContent()
     }
 
+    @Autobind
+    dragOverHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul')!
+        listEl.classList.add('droppable')        
+    }
+
+    dropHandler(_: DragEvent): void {
+        
+    }
+
+    @Autobind
+    dragLeaveHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul')!
+        listEl.classList.remove('droppable')
+    }
+
     configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler)
+        this.element.addEventListener('dragleave', this.dragLeaveHandler)
+        this.element.addEventListener('drop', this.dropHandler)
+        
         projectState.addListeners((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
                 if (this.type === 'active') {
