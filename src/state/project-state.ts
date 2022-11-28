90c7d7b8b48516e1d0c1985be1fa4b10e51113f2
olaide-hok <olaidekareem43@yahoo.com>
2022-11-28 16:44:59 +0100

    Remove namespace App wrapper.
    Use ES6 module import syntax.

--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,62 +1,63 @@
-namespace App {
-  // Project State Management
+import { Project } from "../models/project.js";
+import { ProjectStatus } from "../models/project.js";
 
-  // Generic Custom type for Listeners
-  type Listener<T> = (items: T[]) => void;
+// Project State Management
 
-  // Reusable State Class
-  class State<T> {
-    protected listeners: Listener<T>[] = []; // An array of function references.
-    // 'protected' keyword is another access modifier similar to 'private' but also allows for access in inheriting classes.
+// Generic Custom type for Listeners
+type Listener<T> = (items: T[]) => void;
 
-    addListeners(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn);
-    }
+// Reusable State Class
+class State<T> {
+  protected listeners: Listener<T>[] = []; // An array of function references.
+  // 'protected' keyword is another access modifier similar to 'private' but also allows for access in inheriting classes.
+
+  addListeners(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+export class ProjectState extends State<Project> {
+  private constructor() {
+    super();
   }
 
-  export class ProjectState extends State<Project> {
-    private constructor() {
-      super();
-    }
+  private projects: Project[] = [];
+  private static instance: ProjectState;
 
-    private projects: Project[] = [];
-    private static instance: ProjectState;
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      }
-      this.instance = new ProjectState();
+  static getInstance() {
+    if (this.instance) {
       return this.instance;
     }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
 
-    addProject(title: string, description: string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        ProjectStatus.Active
-      );
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Active
+    );
 
-      this.projects.push(newProject);
+    this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
       this.updateListeners();
     }
+  }
 
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-      const project = this.projects.find((prj) => prj.id === projectId);
-      if (project && project.status !== newStatus) {
-        project.status = newStatus;
-        this.updateListeners();
-      }
-    }
-
-    private updateListeners() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.projects.slice());
-      }
+  private updateListeners() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
     }
   }
-  // Global Project State instance
-  export const projectState = ProjectState.getInstance(); // This ensures only one instance of this state manager is created.
 }
+// Global Project State instance
+export const projectState = ProjectState.getInstance(); // This ensures only one instance of this state manager is created.
