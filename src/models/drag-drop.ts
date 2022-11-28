e6555ca5d1f6b3e5e4e4d8ca44437ada52f8b281
olaide-hok <olaidekareem43@yahoo.com>
2022-11-28 16:48:15 +0100

    Remove namespace App wrapper.

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,13 +1,11 @@
 // Drop and Drag Interfaces which can be used as a construct on a class
-namespace App {
-  export interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-    dragEndHandler(event: DragEvent): void;
-  }
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
 
-  export interface DragTarget {
-    dragOverHandler(event: DragEvent): void; // Permits the drop
-    dropHandler(event: DragEvent): void; // Handles the drop
-    dragLeaveHandler(event: DragEvent): void; // Updates UI
-  }
+export interface DragTarget {
+  dragOverHandler(event: DragEvent): void; // Permits the drop
+  dropHandler(event: DragEvent): void; // Handles the drop
+  dragLeaveHandler(event: DragEvent): void; // Updates UI
 }
